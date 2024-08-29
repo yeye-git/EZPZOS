4536c4ec14b93be2ea9723f7c9d679becc0d0bc3
yeye <1010386305@qq.com>
2024-08-30 00:14:03 +1000

    update code:

--- EZPZOS.Web/src/index.tsx
+++ EZPZOS.Web/src/index.tsx
@@ -1,20 +1,21 @@
-import React from "react";
 import ReactDOM from "react-dom/client";
-import { Provider } from "react-redux";
-import App from "./App";
-import { store } from "./Store/Store";
 import "./index.css";
 import reportWebVitals from "./reportWebVitals";
 
-const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
-root.render(
-	<React.StrictMode>
-		<Provider store={store}>
-			<App />
-		</Provider>
-	</React.StrictMode>
-);
+// const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
+// root.render(
+// 	<React.StrictMode>
+// 		<Provider store={store}>
+// 			<App />
+// 		</Provider>
+// 	</React.StrictMode>
+// );
 
+const rootElement = document.getElementById("root");
+if (!rootElement) {
+    throw new Error("Root element not found");
+}
+const root = ReactDOM.createRoot(rootElement);
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
